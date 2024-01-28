441063b418e0f8f2aa8c61c9b3ef8da22722c802
Joshua Padman <joshua@padman.id.au>
2024-01-28 20:19:44 +1100

    Enter on password textboxes
    There has to be a better way to pass config, surely!

--- src/ui/settings/general.rs
+++ src/ui/settings/general.rs
@@ -1,3 +1,6 @@
+use floem::event::{Event, EventListener};
+use floem::keyboard::{KeyCode, PhysicalKey};
+use floem::reactive::RwSignal;
 use floem::widgets::button;
 use floem::{
 	reactive::create_rw_signal,
@@ -24,6 +27,9 @@ pub fn general_view(
 	let new_password_check = create_rw_signal(String::from(""));
 	let password_error = create_rw_signal(String::from(""));
 	let password_config = config.clone();
+	let new_pass_again_config = config.clone();
+	let old_pass_config = config.clone();
+	let new_pass_config = config.clone();
 
 	let debug_settings_slot = if std::env::var("DEBUG").is_ok() {
 		let is_encrypted = create_rw_signal(config.config_db.read().encrypted);
@@ -63,27 +69,78 @@ pub fn general_view(
 		label(|| "Change password:"),
 		h_stack((
 			label(|| "Old Password"),
-			container(password_field(old_password, "")),
-			label(|| "New Password"),
-			container(password_field(new_password, "")),
-			label(|| "New Password Again"),
-			container(password_field(new_password_check, "")),
-			label(move || password_error.get()).style(|s| s.color(C_ERROR)),
-			container(button(|| "Update Password").on_click(move |_| {
-				if new_password.get() != new_password_check.get() {
-					password_error.set(String::from("New passwords do not match"));
-					return EventPropagation::Continue;
-				}
-				let result = password_config
-					.change_password(old_password.get(), new_password.get());
-				match result {
-					Ok(()) => {
-						password_error.set(String::from("Password updated successfully"))
+			container(password_field(old_password, "").on_event(
+				EventListener::KeyDown,
+				move |event| {
+					let key = match event {
+						Event::KeyDown(k) => k.key.physical_key,
+						_ => PhysicalKey::Code(KeyCode::F35),
+					};
+					if key == PhysicalKey::Code(KeyCode::Enter) {
+						return change_password(
+							old_pass_config.clone(),
+							old_password,
+							new_password,
+							new_password_check,
+							password_error,
+						);
 					}
-					Err(e) => password_error.set(e.to_string()),
-				}
-				EventPropagation::Continue
-			})),
+					EventPropagation::Continue
+				},
+			)),
+			label(|| "New Password"),
+			container(password_field(new_password, "").on_event(
+				EventListener::KeyDown,
+				move |event| {
+					let key = match event {
+						Event::KeyDown(k) => k.key.physical_key,
+						_ => PhysicalKey::Code(KeyCode::F35),
+					};
+					if key == PhysicalKey::Code(KeyCode::Enter) {
+						return change_password(
+							new_pass_config.clone(),
+							old_password,
+							new_password,
+							new_password_check,
+							password_error,
+						);
+					}
+					EventPropagation::Continue
+				},
+			)),
+			label(|| "New Password Again"),
+			container(password_field(new_password_check, "").on_event(
+				EventListener::KeyDown,
+				move |event| {
+					let key = match event {
+						Event::KeyDown(k) => k.key.physical_key,
+						_ => PhysicalKey::Code(KeyCode::F35),
+					};
+					if key == PhysicalKey::Code(KeyCode::Enter) {
+						return change_password(
+							new_pass_again_config.clone(),
+							old_password,
+							new_password,
+							new_password_check,
+							password_error,
+						);
+					}
+					EventPropagation::Continue
+				},
+			)),
+			container(label(|| "")),
+			label(move || password_error.get()).style(|s| s.color(C_ERROR)),
+			container(label(|| "")),
+			container(button(|| "Update Password").on_click(move |_| {
+				change_password(
+					password_config.clone(),
+					old_password,
+					new_password,
+					new_password_check,
+					password_error,
+				)
+			}))
+			.style(|s| s.justify_end()),
 		))
 		.style(styles::settings_line),
 	)))
@@ -98,4 +155,23 @@ pub fn general_view(
 		.style(|s| s.width_full()),
 	)
 	.style(|s| s.width_full().min_width(210))
+}
+
+fn change_password(
+	config: Config,
+	old_password: RwSignal<String>,
+	new_password: RwSignal<String>,
+	new_password_check: RwSignal<String>,
+	password_error: RwSignal<String>,
+) -> EventPropagation {
+	if new_password.get() != new_password_check.get() {
+		password_error.set(String::from("New passwords do not match"));
+		return EventPropagation::Continue;
+	}
+	let result = config.change_password(old_password.get(), new_password.get());
+	match result {
+		Ok(()) => password_error.set(String::from("Password updated successfully")),
+		Err(e) => password_error.set(e.to_string()),
+	}
+	EventPropagation::Continue
 }
