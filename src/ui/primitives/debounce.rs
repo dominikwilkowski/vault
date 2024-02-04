1201d742d98a16fca518b3b63804d50639e46412
Joshua Padman <jpadman@redhat.com>
2024-02-05 10:16:17 +1100

    Here is an atomicu64 version

--- src/ui/primitives/debounce.rs
+++ src/ui/primitives/debounce.rs
@@ -1,73 +1,33 @@
 use floem::action::exec_after;
-use parking_lot::RwLock;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::{sync::Arc, time::Duration};
 
 #[derive(Debug, Clone)]
 pub struct Debounce {
 	duration: Duration,
-	counter: Arc<RwLock<u64>>,
+	counter: Arc<AtomicU64>,
 }
 
 impl Default for Debounce {
 	fn default() -> Self {
 		Debounce {
 			duration: Duration::from_millis(100),
-			counter: Arc::new(RwLock::new(0)),
+			counter: Arc::new(AtomicU64::new(0)),
 		}
 	}
 }
 
 impl Debounce {
 	pub fn add(self, action: impl Fn() + 'static) {
-		let call_counter: u64;
-		{
-			let mut counter = self.counter.write();
-			*counter += 1;
-			call_counter = *counter;
-		}
+		let mut call_counter = self.counter.fetch_add(1, Ordering::Relaxed);
+		call_counter += 1;
 		exec_after(self.duration, move |_| {
 			// If the counter set in this call is the same as in Debounce then it is the latest within duration.
-			if *self.counter.read() == call_counter {
+			if self.counter.load(Ordering::Relaxed) == call_counter {
 				// Reset the counter
-				*self.counter.write() = 0;
+				self.counter.store(0, Ordering::Relaxed);
 				action();
 			}
 		});
 	}
 }
-
-// use floem::action::exec_after;
-// use parking_lot::RwLock;
-// use std::{sync::Arc, time::Duration};
-//
-// #[derive(Debug, Clone)]
-// pub struct Debounce<T> {
-// 	pub duration: Duration,
-// 	pub data: Arc<RwLock<Vec<T>>>,
-// }
-//
-// impl<T> Default for crate::ui::primitives::debounce::Debounce<T> {
-// 	fn default() -> Self {
-// 		crate::ui::primitives::debounce::Debounce {
-// 			duration: Duration::from_millis(100),
-// 			data: Arc::new(RwLock::new(vec![])),
-// 		}
-// 	}
-// }
-//
-// impl<T: std::fmt::Debug + PartialEq + Clone + 'static> crate::ui::primitives::debounce::Debounce<T> {
-// 	pub fn add_value(self, value: T, action: impl Fn() + 'static) {
-// 		let value_fn = value.clone();
-// 		{
-// 			self.data.write().push(value);
-// 		}
-//
-// 		exec_after(self.duration, move |_| {
-// 			if self.data.read().ends_with(&[value_fn.clone()]) {
-// 				// If the value that was sent is not the same as the top of the vec then it is a
-// 				// nop. This might "double save" on occasion, depending on the data.
-// 				action();
-// 			}
-// 		});
-// 	}
-// }
