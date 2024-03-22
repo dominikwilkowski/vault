92d3aeee576a6c5e824fa466132ebdb5d7eda10a
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2024-03-23 06:24:59 +1000

    actually read clippy thread_local_initializer_can_be_made_const and fixed it

--- src/ui/window_management.rs
+++ src/ui/window_management.rs
@@ -12,8 +12,7 @@ use floem::{
 use crate::db::DbFields;
 
 thread_local! {
-	#[allow(clippy::thread_local_initializer_can_be_made_const)]
-	pub(crate) static OPEN_WINDOWS: RefCell<Vec<(String, WindowId)>> = RefCell::new(Vec::new());
+	pub(crate) static OPEN_WINDOWS: RefCell<Vec<(String, WindowId)>> = const { RefCell::new(Vec::new()) };
 }
 
 pub fn make_field_path(id: usize, field: &DbFields) -> String {
