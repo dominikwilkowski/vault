a8883384a1713baa8ff29eb45c7bfe6acb25a1fd
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2024-04-06 08:18:40 +1000

    fixed modulo bias, added bias towards letters and numbers over special characters

--- src/password_gen.rs
+++ src/password_gen.rs
@@ -1,4 +1,4 @@
-use rand::{rngs::OsRng, seq::SliceRandom, Rng};
+use rand::{rngs::OsRng, Rng};
 use sha2::{Digest, Sha256};
 
 use floem::reactive::use_context;
@@ -25,16 +25,36 @@ pub fn generate_password(entropy: String) -> String {
 	rng.fill(&mut hash_array);
 
 	// Define the character set for the random string
-	let charset: [char; 88] = [
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
-		'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-		'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-		'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-		'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+',
-		'=', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '/',
+	#[rustfmt::skip]
+	let charset: [char; 160] = [
+		// 0-1 three times so we get some numbers
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+
+		// a-bA-Z
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+		'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+
+		// a-b twice to bias over special characters
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+
+		// special characters
+		'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '[',
+		']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '/',
 	];
 
 	// Generate a string of n characters
 	let length = env.config.general.read().pass_gen_letter_count;
-	(0..length).map(|_| *charset.choose(&mut rng).unwrap()).collect()
+	(0..length)
+		.map(|_| {
+			let index = rng.gen_range(0..charset.len());
+			charset[index]
+		})
+		.collect()
 }
