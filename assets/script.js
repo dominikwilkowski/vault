1d4d781bc8dd0c886db4333d43960e96e1c523a5
dominikwilkowski <dominikwilkowski@users.noreply.github.com>
2024-05-26 07:40:25 +0000

    deploy: bec56767dfa1dc2b208cf19f4d9831e1d0864b72

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,36 @@
-function getOS() {
-	const userAgent = window.navigator.userAgent;
-	const platform = window.navigator?.userAgentData?.platform || window.navigator.platform;
-	const macosPlatforms = ['macOS', 'Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
-	const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+function get_os() {
+	const platform = (window.navigator?.userAgentData?.platform || window.navigator.platform).toLowerCase();
+	const macos_platforms = ["macos", "macintosh", "macintel", "macppc", "mac68k"];
+	const windows_platforms = ["win32", "win64", "windows", "wince"];
 
-	if (macosPlatforms.indexOf(platform) !== -1) {
-		return 'macOS';
-	} else if (windowsPlatforms.indexOf(platform) !== -1) {
-		return 'Windows';
+	if (macos_platforms.indexOf(platform) !== -1) {
+		return "macOS";
+	} else if (windows_platforms.indexOf(platform) !== -1) {
+		return "Windows";
 	} else if (/Linux/.test(platform)) {
-		return 'Linux';
+		return "Linux";
 	}
 
-	return 'unknown';
+	return "macOS";
 }
+
+let os = get_os();
+let link = document.querySelector("#"+ os +"_link").getAttribute('href');
+document.querySelector("#cta").setAttribute('href', link);
+
+document.querySelector("#macOS_icon").classList.add("hidden");
+document.querySelector("#Windows_icon").classList.add("hidden");
+document.querySelector("#Linux_icon").classList.add("hidden");
+document.querySelector("#" + os + "_icon").classList.remove("hidden");
+
+
+document.querySelector("#marketing-bs-btn").addEventListener('click', () => {
+	let body = document.querySelector("body");
+	if (body.classList.contains("marketing-bs")) {
+		body.classList.remove("marketing-bs");
+		body.classList.add("plain");
+	} else {
+		body.classList.remove("plain");
+		body.classList.add("marketing-bs");
+	}
+});
